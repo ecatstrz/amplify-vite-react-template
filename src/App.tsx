261c689d99a1a8637e74f5ba07b2b7d04d4d542e
caterinaQDev <ecatstrz@amazon.com>
2024-06-28 20:18:34 +0000

    getting the cat on the deploy

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@
 
 // export default App;
 // import React from 'react';
-import catImage from '/projects/amplify-vite-react-template/src/assets/c2ba9d93-5267-4ac5-9f61-78c83a0d8667.jpg';
+import catImage from './assets/c2ba9d93-5267-4ac5-9f61-78c83a0d8667.jpg';
 // import catImage from 's3://amplify-d3tvty808zfcjf-ma-amplifydataamplifycodege-o6mwyb4yj1zc/cat.jpg';
 
 function App() {
