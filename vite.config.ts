2b747e2822168d42d570a0d568e4c4c226e1d4b0
caterinaQDev <ecatstrz@amazon.com>
2024-06-28 19:24:42 +0000

    rollupfun

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['/projects/amplify-vite-react-template/src/assets/c2ba9d93-5267-4ac5-9f61-78c83a0d8667.jpg'],
+    },
+  },
 })
