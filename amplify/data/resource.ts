cbc82568d9fb97cf5d953d1d9d7bff8a0abdebec
caterinaQDev <ecatstrz@amazon.com>
2024-07-05 18:41:37 +0000

    backend change, should update the amplify resources json

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,9 +7,10 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
+  Cat: a
     .model({
-      content: a.string()
+      content: a.string(),
+      imageUrl: a.string()
     })
     .authorization((allow) => [allow.publicApiKey()]),
 });
